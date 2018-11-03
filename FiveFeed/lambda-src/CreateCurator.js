70ca58d3197d2354f1be5f3bfbe5b32b7b66ec85
NEsfandiari <nesfandiari37@gmail.com>
2018-11-03 16:59:01 -0700

    createCurator function

--- FiveFeed/lambda-src/CreateCurator.js
+++ FiveFeed/lambda-src/CreateCurator.js
@@ -1,12 +1,38 @@
 require('dotenv').config({ path: '.env.development' })
-const axios = require('axios')
 const statusCode = 200
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
 const faunadb = require('faunadb')
+const q = faunadb.query
 
 exports.handler = async function(event, context, callback) {
+  let data = JSON.parse(event.body)
+  data = JSON.parse(data.body)
   const client = new faunadb.Client({ secret: process.env.faunaKey })
+  client
+    .query(
+      q.Create(q.Class('curators'), {
+        data: { name: data.name },
+      })
+    )
+    .then(res => {
+      console.log(res)
+      let response = {
+        statusCode,
+        headers,
+        body: 'Success',
+      }
+      callback(null, response)
+    })
+    .catch(res => {
+      console.log(res)
+      let response = {
+        statusCode,
+        headers,
+        body: 'failure',
+      }
+      callback(null, response)
+    })
 }
