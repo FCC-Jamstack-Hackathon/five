fc254325163c77549dce3679f14f99cffe6f18fc
NEsfandiari <nesfandiari37@gmail.com>
2018-11-03 16:02:49 -0700

    faunkey setup

--- FiveFeed/lambda-src/CreateCurator.js
+++ FiveFeed/lambda-src/CreateCurator.js
@@ -5,5 +5,8 @@ const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
+const faunadb = require('faunadb')
 
-exports.handler = async function(event, context, callback) {}
+exports.handler = async function(event, context, callback) {
+  const client = new faunadb.Client({ secret: process.env.faunaKey })
+}
