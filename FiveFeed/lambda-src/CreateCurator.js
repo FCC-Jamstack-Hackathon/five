876f95b1abd75c3d33eb338a07791f0643d7f784
NEsfandiari <nesfandiari37@gmail.com>
2018-11-03 15:36:01 -0700

    lambda function boilerplate

---
+++ FiveFeed/lambda-src/CreateCurator.js
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: '.env.development' })
+const axios = require('axios')
+const statusCode = 200
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
+
+exports.handler = async function(event, context, callback) {}
