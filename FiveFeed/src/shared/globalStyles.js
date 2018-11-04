b808f7f097b69836dc451f6c862616bec6c8e974
Frank Faustino <nawzyah@gmail.com>
2018-11-03 17:18:00 -0700

    Retest font import

--- FiveFeed/src/shared/globalStyles.js
+++ FiveFeed/src/shared/globalStyles.js
@@ -1,26 +1,26 @@
 import { createGlobalStyle } from 'styled-components'
-import fonts from './fonts'
+import DankMonoRegular from '../fonts/dankmono-regular.woff'
+import DankMonoItalic from '../fonts/dankmono-italic.woff'
 
-const textColor = '#D6DEEB'
+// const textColor = '#D6DEEB'
 const first = '#ADDB67'
 const second = '#011627'
-const third = '#122D42'
-const fourth = '#32374C'
-
+// const third = '#122D42'
+// const fourth = '#32374C'
 
 export default createGlobalStyle`
   @font-face {
     font-family: "Dank-Mono";
     font-style: normal;
     font-weight: normal;
-    src: local("Dank Mono"), local("Dank-Mono"), url(${fonts.DankMonoRegular}) format("woff");
+    src: local("Dank Mono"), local("Dank-Mono"), url(${DankMonoRegular}) format("woff");
   }
 
   @font-face {
     font-family: "Dank-Mono";
     font-style: italic;
     font-weight: normal;
-    src: local("Dank Mono"), local("Dank-Mono"), url(${fonts.DankMonoItalic}) format("woff");
+    src: local("Dank Mono"), local("Dank-Mono"), url(${DankMonoItalic}) format("woff");
   }
 
   body {
