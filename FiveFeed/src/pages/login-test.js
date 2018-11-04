201dba029e0bd8ba04fd69057c27f76057879693
Justin Kaseman <justinkaseman@live.com>
2018-11-04 11:12:28 -0800

    Login ready for Fauna GET

--- FiveFeed/src/pages/login-test.js
+++ FiveFeed/src/pages/login-test.js
@@ -6,8 +6,33 @@ import { Link } from 'gatsby'
 import netlifyIdentity from 'netlify-identity-widget'
 
 class LoginTest extends Component {
-  clickHandler = () => {
+  generateHeaders() {
+    const headers = { 'Content-Type': 'application/json' }
+    if (netlifyIdentity.currentUser()) {
+      return netlifyIdentity
+        .currentUser()
+        .jwt()
+        .then(token => {
+          return { ...headers, Authorization: `Bearer ${token}` }
+        })
+    }
+    return Promise.resolve(headers)
+  }
+
+  clickHandler = e => {
+    e.preventDefault()
     netlifyIdentity.open()
+    netlifyIdentity.on('login', user => console.log('login', user))
+    // store token in localstorage
+    this.generateHeaders().then(headers => {
+      // GET request
+      //
+      //   USE IN LAMBDA:
+      // const claims = context.clientContext && context.clientContext.user;
+      //   if (!claims) {
+      //     return callback(null, { statusCode: 401, body: "You must be signed in to call this function" });
+      //   }
+    })
   }
   render() {
     return (
