7d16a230edee66a1ee3ade3318270936634936ee
Frank Faustino <nawzyah@gmail.com>
2018-11-05 15:11:27 -0800

    Fix Header spacing

--- FiveFeed/src/components/header.js
+++ FiveFeed/src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import LoginButton from './login'
@@ -7,14 +8,14 @@ import LoginButton from './login'
 const Container = styled.div`
   margin: 0 auto;
   maxwidth: 960;
-  padding: 1rem 0.5rem;
+  padding: 1rem 0.5rem 0.75rem 0.5rem;
   display: grid;
   grid-template-columns: 1fr 75% 1fr;
   justify-content: center;
 
   @media (min-width: 768px) {
     & {
-      padding: 2rem 1rem;
+      padding: 2rem 1rem 1rem 1rem;
     }
   }
 `
@@ -22,8 +23,7 @@ const Title = styled.h1`
   grid-column-start: 2;
   margin: 0;
   text-align: center;
-`
-
+  `
 const StyledLink = styled(Link)`
   color: #addb67;
 `
@@ -38,5 +38,7 @@ const Header = ({ siteTitle }) => (
     <LoginButton />
   </Container>
 )
+
+Header.propTypes = { siteTitle: PropTypes.string.isRequired }
 
 export default Header
