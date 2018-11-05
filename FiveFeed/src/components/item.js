7fba89fd01004e19ffab93a8b1f118fe5774ecd1
Frank Faustino <nawzyah@gmail.com>
2018-11-05 15:12:19 -0800

    Add new icons

--- FiveFeed/src/components/item.js
+++ FiveFeed/src/components/item.js
@@ -1,39 +1,44 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
-import Audio from '../icons/audio'
-import Book from '../icons/book'
-import JS from '../icons/js'
-import ReactIcon from '../icons/react'
-import Tech from '../icons/tech'
+import { Article, Audio, Book, Code, Event, Instagram, JS, ReactIcon, Tech, Video } from '../icons'
 
 const IconPicker = type => {
   switch (type) {
-    case 'react':
-      return <ReactIcon />
-    case 'js':
-      return <JS />
-    case 'book':
-      return <Book />
     case 'audio':
       return <Audio />
-    default:
+    case 'book':
+      return <Book />
+    case 'code':
+      return <Code />
+    case 'event':
+      return <Event />
+    case 'instagram':
+      return <Instagram />
+    case 'js':
+      return <JS />
+    case 'react':
+      return <ReactIcon />
+      case 'tech':
       return <Tech />
+    case 'video':
+      return <Video />
+    default:
+      return <Article />
   }
 }
 
 const Item = ({ children, type, url }) => (
   <div className="item-container">
     {IconPicker(type)}
-    <Link to={url}>{children}</Link>
+    <a href={url}>{children}</a>
   </div>
 )
 
 Item.propTypes = {
   children: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
 }
 
 export default Item
